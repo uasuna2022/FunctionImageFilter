8ee39ca4fac5ba0c96bdeb2a78ce9b44471f4a69
uasuna2022 <max.gomanchuk@gmail.com>
2025-12-10 19:55:14 +0100

    PixelProcessor delegate defined

---
+++ Project3_CustomFunctionImageFilter/Delegates.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3_CustomFunctionImageFilter
+{
+    public delegate (byte oldR, byte oldG, byte oldB) PixelProcessor(byte newR, byte newG, byte newB);
+}
