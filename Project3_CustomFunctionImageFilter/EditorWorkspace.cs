7af53a1fbb6d3beab4cdcab900249e4f17fc868f
uasuna2022 <max.gomanchuk@gmail.com>
2025-12-10 02:06:56 +0100

    Check/scroll events handled partially

---
+++ Project3_CustomFunctionImageFilter/EditorWorkspace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3_CustomFunctionImageFilter
+{
+    public sealed class EditorWorkspace
+    {
+        private static EditorWorkspace? _instance;
+        public static EditorWorkspace Instance => _instance ??= new EditorWorkspace();
+        private EditorWorkspace() { }
+        public float Gamma { get; set; } = 1.0F; // [0.1; 5.0]
+        public int Brightness { get; set; } = 0; // [-230; 230]
+        public int Contrast { get; set; } = 0;   // [-127; 127]
+
+        public bool NoFilter { get; set; } = true;
+        public bool NegationFilter { get; set; } = false;
+        public bool CustomFunctionFilter { get; set; } = false;
+        public bool GammaFilter { get; set; } = false;
+        public bool BrightnessFilter { get; set; } = false;
+        public bool ContrastFilter { get; set; } = false;
+
+
+    }
+}
--- Project3_CustomFunctionImageFilter/MainWindow.cs
+++ Project3_CustomFunctionImageFilter/MainWindow.cs
@@ -7,6 +7,87 @@ namespace Project3_CustomFunctionImageFilter
             InitializeComponent();
         }
 
+        private void UpdateCheckProperties()
+        {
+            EditorWorkspace.Instance.CustomFunctionFilter = customFunctionRadioButton.Checked;
+            EditorWorkspace.Instance.ContrastFilter = contrastFilterRadioButton.Checked;
+            EditorWorkspace.Instance.BrightnessFilter = brightnessFilterRadioButton.Checked;
+            EditorWorkspace.Instance.GammaFilter = gammaCorrectionFilterRadioButton.Checked;
+            EditorWorkspace.Instance.NoFilter = noFilterRadioButton.Checked;
+            EditorWorkspace.Instance.NegationFilter = negationFilterRadioButton.Checked;
+        }
+        private void UpdateEnableProperties()
+        {
+            brightnessFilterTrackBar.Enabled = brightnessFilterRadioButton.Checked;
+            contrastFilterTrackBar.Enabled = contrastFilterRadioButton.Checked;
+            gammaCorrectionFilterTrackBar.Enabled = gammaCorrectionFilterRadioButton.Checked;
+        }
 
+        private void noFilterRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCheckProperties();
+            UpdateEnableProperties();
+            // TODO: apply changes
+        }
+
+        private void negationFilterRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCheckProperties();
+            UpdateEnableProperties();
+            // TODO: apply changes
+        }
+
+        private void customFunctionRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCheckProperties();
+            UpdateEnableProperties();
+            // TODO: if checked, open a helper window to define a function
+            // TODO: apply changes
+        }
+
+        private void brightnessFilterRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCheckProperties();
+            UpdateEnableProperties();
+            // TODO: apply changes
+        }
+
+        private void contrastFilterRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCheckProperties();
+            UpdateEnableProperties();
+            // TODO: apply changes
+        }
+
+        private void gammaCorrectionFilterRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCheckProperties();
+            UpdateEnableProperties();
+            // TODO: apply changes
+        }
+
+        private void brightnessFilterTrackBar_Scroll(object sender, EventArgs e)
+        {
+            int value = brightnessFilterTrackBar.Value;
+            EditorWorkspace.Instance.Brightness = value;
+            brightnessValueLabel.Text = value.ToString();
+            // TODO: apply changes
+        }
+
+        private void contrastFilterTrackBar_Scroll(object sender, EventArgs e)
+        {
+            int value = contrastFilterTrackBar.Value;
+            EditorWorkspace.Instance.Contrast = value;
+            contrastValueLabel.Text = value.ToString();
+            // TODO: apply changes
+        }
+
+        private void gammaCorrectionFilterTrackBar_Scroll(object sender, EventArgs e)
+        {
+            float value = (float)gammaCorrectionFilterTrackBar.Value / 10.0F;
+            EditorWorkspace.Instance.Gamma = value;
+            gammaCorrectionValueLabel.Text = value.ToString("F1");
+            // TODO: apply changes
+        }
     }
 }
